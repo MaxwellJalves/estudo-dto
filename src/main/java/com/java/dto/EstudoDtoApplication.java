928a3591c075b0fac6c0ad13c09d7b1514d98bed
MaxwellJAlves <maxwell880@gmail.com>
2021-05-30 15:03:26 -0300

    projeto inicial

---
+++ src/main/java/com/java/dto/EstudoDtoApplication.java
@@ -0,0 +1,13 @@
+package com.java.dto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoDtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoDtoApplication.class, args);
+	}
+
+}
